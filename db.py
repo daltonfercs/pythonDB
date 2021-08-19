741cac044c30a493e7f10db2c7f4302b0aa0034f
Dalton Celadita <dafecesa.dfcs@gmail.com>
2021-08-19 18:41:49 -0500

    insertar datos

--- db.py
+++ db.py
@@ -9,7 +9,21 @@ mydb = mysql.connector.connect(
 )
 cursor  =   mydb.cursor()
 
-cursor.execute('select * from Usuario')
-resultado = cursor.fetchall()
+#listar db
+#cursor.execute('select * from Usuario')
+#resultado = cursor.fetchall()
 
-print(resultado)
+#ver definiciones de datos
+#cursor.execute('show create table Usuario')
+#resultado = cursor.fetchall()
+
+#Insertar datos
+sql =   'insert into Usuario(nombre, email) values(%s, %s)'
+values =    ('dalstrom', 'dalstrom@dfcs.dc')
+
+cursor.execute(sql, values)
+mydb.commit()
+
+
+#print(resultado)
+print(cursor.rowcount)
